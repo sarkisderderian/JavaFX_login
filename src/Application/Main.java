9830c8ad292ea55244ab30308aecdaa5c6efb174
epc-sarkis <epc-sarkis@169.254.164.98>
2014-02-20 14:16:09 +0400

    add the button

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -14,10 +14,12 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -56,6 +58,12 @@ public class Main extends Application {
 		
 		PasswordField pwBox=new PasswordField();
 		grid.add(pwBox, 1, 2);
+                
+                Button btn=new Button("Sign in");
+                HBox hbBtn=new HBox(10);
+                hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+                hbBtn.getChildren().add(btn);
+                grid.add(hbBtn, 1, 4);
 		
 		grid.setGridLinesVisible(true);
 		primaryStage.setScene(scene);
